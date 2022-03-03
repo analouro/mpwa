44ff2c8de6d6d9af686945ed8fdc3bad343569d4
Ubuntu <ana@ana-machine.gsc4kn5ep5bujc2fl04gwmbk4a.zx.internal.cloudapp.net>
2022-03-03 16:37:10 +0000

    Routes are working: home, recipe, log - however db isn't working

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,8 @@
 from crypt import methods
 from flask import Flask, render_template, request
 from application import app
-from application.forms import UserForm
+from application.forms import RecipeForm, UserForm
+from application.models import User, Recipe
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
@@ -18,3 +19,26 @@ def home():
             message = f'Welcome {user_name}'
     
     return render_template('home.html', form=form, message=message)
+
+@app.route('/recipe', methods=['GET', 'POST'])
+def recipe():
+    message = ""
+    form = RecipeForm()
+
+    if request.method == 'POST':
+        recipe_name = form.recipe_name.data
+
+        if len(recipe_name) == 0:
+            message = "Please add a new recipe to continue"
+        else:
+            message = f'{recipe_name} was added to your recipe log'
+    
+    return render_template('recipe.html', form=form, message=message)
+
+@app.route('/log')
+def log():
+    recipes = Recipe.query.all()
+    return render_template('log.html', recipes=recipes)
+
+
+
