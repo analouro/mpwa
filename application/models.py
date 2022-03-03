03643b6a1766dc5ef2d5700c0878b21b283f465b
Ubuntu <ana@development-server.gsc4kn5ep5bujc2fl04gwmbk4a.zx.internal.cloudapp.net>
2022-03-03 12:36:12 +0000

    Coded models.py

---
+++ application/models.py
@@ -0,0 +1,15 @@
+from application import db
+
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(50), nullable=False)
+
+class Recipe(db.Model):
+    recipe_id = db.Column(db.Integer, primary_key=True)
+    fuser_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    recipe_name = db.Column(db.String(100), nullable=False)
+    recipe_ing = db.Column(db.String(500), nullable=True)
+    recipe_servs = db.Column(db.Integer, nullable=True)
+    recipe_guide = db.Column(db.String(2000), nullable=True)
+
+
