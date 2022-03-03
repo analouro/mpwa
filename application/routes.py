02404efb445cf27969a19c49b13e094b6d3d3421
Ubuntu <ana@ana-machine.gsc4kn5ep5bujc2fl04gwmbk4a.zx.internal.cloudapp.net>
2022-03-03 14:49:24 +0000

    Changes to routes.py

--- application/routes.py
+++ application/routes.py
@@ -1,11 +1,12 @@
 from crypt import methods
-from flask import Flask
+from flask import Flask, render_template, request
 from application import app
 from application.forms import UserForm
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
 def home():
+    message = ""
     form = UserForm()
 
     if request.method == 'POST':
