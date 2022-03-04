adf67d76ab2b99aa9056efc9fcb3643b5ac58df7
Ubuntu <ana@ana-machine.gsc4kn5ep5bujc2fl04gwmbk4a.zx.internal.cloudapp.net>
2022-03-04 15:11:49 +0000

    Deleted most of the routes.py file + recipe.html file - stuck / starting over

--- application/forms.py
+++ application/forms.py
@@ -7,11 +7,11 @@ from application.models import Recipe
 
 # class to enter username
 class UserForm(FlaskForm):
-    user_name = StringField('Enter Username', validators=[DataRequired(), Length(min=1,max=50)])
+    user_name = StringField('Enter Username', validators=[DataRequired(), Length(min=2,max=50)])
     submit_name = SubmitField('Add Username')
 
 # class to enter recipe name
 class RecipeForm(FlaskForm):
-    recipe_name = StringField('Enter the name of your Recipe', validators=[DataRequired(), Length(min=1,max=100)])
+    recipe_name = StringField('Enter the name of your Recipe', validators=[DataRequired(), Length(min=2,max=100)])
     submit_recipe = SubmitField('Add Recipe')
 
--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,3 @@
-from crypt import methods
-from email import message
 from flask import Flask, redirect, render_template, request, url_for
 from application import app, db
 from application.forms import RecipeForm, UserForm
@@ -10,37 +8,53 @@ from application.models import User, Recipe
 def home():
     userform = UserForm()
 
-    if userform.validate_on_submit():
-        user_name = User(user_name=userform.user_name.data)
-        db.session.add(user_name)
-        db.session.commit()
-          
+    if request.method == 'POST':
+        if userform.validate_on_submit():
+            user_name = User(user_name=userform.user_name.data)
+            db.session.add(user_name)
+            db.session.commit()
+            return redirect(url_for('recipe'))
+
+    #if the username isn't validated, return to home      
     return render_template('home.html', form=userform)
 
-# @app.route('/recipe', methods=['GET', 'POST'])
-# def recipe():
-#     # message = ""
-#     recipeform = RecipeForm()
+@app.route('/recipe', methods=['GET', 'POST'])
+#@app.route('/recipe/<name>', methods=['GET', 'POST'])
+def recipe():
 
-#     if recipeform.validate_on_submit():
-#         recipe_name = Recipe(recipe_name=recipeform.recipe_name.data)
-#         db.session.add(recipe_name)
-#         db.session.commit()
-#         # if a recipe is added successfully, the user is redirected to the recipe log
-#         return render_template('log.html', form=recipeform)
+    # if request.method == 'GET':
+        return render_template('recipe.html')
+    # # userform = UserForm
+    # user = User.query.get(1)
+    # user.user_name = name
+    # db.session.commit()
+    # return user.user_name
+    # # name.user_name = name
 
-#         # if len(recipe_name) == 0:
-#         #     message = "Please add a new recipe to continue"
-#         # else:
-#         #     message = f'{recipe_name} was added to your recipe log'
+    # if request.method == 'GET':
+    #     userform.user_name.data = user.user_name
+    #     return render_template('recipe.html', form=userform)
+        
+    # return render_template ('recipe.html')
+    # recipeform = RecipeForm()
 
-#     #if a recipe isn't added successfully, refresh the recipe page
-#     return render_template('recipe.html', form=recipeform), #message=message)
+    # if recipeform.validate_on_submit():
+    #     user_name = User.query.filter_by()
+    #     recipe_name = Recipe(recipe_name=recipeform.recipe_name.data)
+    #     db.session.add(recipe_name)
+    #     db.session.commit()
+    #     # if a recipe is added successfully, the user is redirected to the recipe log
+    #     return render_template('log.html', form=recipeform)
+
+        # if len(recipe_name) == 0:
+        #     message = "Please add a new recipe to continue"
+        # else:
+        #     message = f'{recipe_name} was added to your recipe log'
+
+    #if a recipe isn't added successfully, refresh the recipe page
+    # return render_template('recipe.html', form=recipeform, user=user_name
 
 # @app.route('/log')
 # def log():
 #     recipes = Recipe.query.all()
 #     return render_template('log.html', recipes=recipes)
-
-
-
