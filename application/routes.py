7c5ba99ab0d16b36c4761e9468334cfa20b33fd4
Ubuntu <ana@ana-machine.gsc4kn5ep5bujc2fl04gwmbk4a.zx.internal.cloudapp.net>
2022-03-04 15:49:02 +0000

    Relationship working between users in home.html and users.html

--- application/routes.py
+++ application/routes.py
@@ -13,48 +13,12 @@ def home():
             user_name = User(user_name=userform.user_name.data)
             db.session.add(user_name)
             db.session.commit()
-            return redirect(url_for('recipe'))
+            return redirect(url_for('users'))
 
     #if the username isn't validated, return to home      
     return render_template('home.html', form=userform)
 
-@app.route('/recipe', methods=['GET', 'POST'])
-#@app.route('/recipe/<name>', methods=['GET', 'POST'])
-def recipe():
-
-    # if request.method == 'GET':
-        return render_template('recipe.html')
-    # # userform = UserForm
-    # user = User.query.get(1)
-    # user.user_name = name
-    # db.session.commit()
-    # return user.user_name
-    # # name.user_name = name
-
-    # if request.method == 'GET':
-    #     userform.user_name.data = user.user_name
-    #     return render_template('recipe.html', form=userform)
-        
-    # return render_template ('recipe.html')
-    # recipeform = RecipeForm()
-
-    # if recipeform.validate_on_submit():
-    #     user_name = User.query.filter_by()
-    #     recipe_name = Recipe(recipe_name=recipeform.recipe_name.data)
-    #     db.session.add(recipe_name)
-    #     db.session.commit()
-    #     # if a recipe is added successfully, the user is redirected to the recipe log
-    #     return render_template('log.html', form=recipeform)
-
-        # if len(recipe_name) == 0:
-        #     message = "Please add a new recipe to continue"
-        # else:
-        #     message = f'{recipe_name} was added to your recipe log'
-
-    #if a recipe isn't added successfully, refresh the recipe page
-    # return render_template('recipe.html', form=recipeform, user=user_name
-
-# @app.route('/log')
-# def log():
-#     recipes = Recipe.query.all()
-#     return render_template('log.html', recipes=recipes)
+@app.route('/users', methods=['GET'])
+def users():
+    user_name = User.query.all()
+    return render_template('users.html', user_name=user_name )
