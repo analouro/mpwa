30ef6f7ea3a03366f5467dff7d95197379f04a15
Ubuntu <ana@ana-machine.gsc4kn5ep5bujc2fl04gwmbk4a.zx.internal.cloudapp.net>
2022-03-09 17:02:09 +0000

    Changes on the deploy.sh - added user

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -2,4 +2,8 @@
 
 echo "Deploy stage"
 
-ssh jenkins@ana-swarm docker stack deploy --compose-file docker-compose.yaml mpwa
+# copying docker-compose.yaml to swarm machine
+scp docker-compose.yaml jenkins@ana-swarm:/home/jenkins/docker-compose.yaml
+
+ssh jenkins@ana-swarm \
+    DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR docker stack deploy --compose-file docker-compose.yaml mpwa
