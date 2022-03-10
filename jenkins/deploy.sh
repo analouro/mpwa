7a4273c44c88f9e8fdddaeea2afdcecea6129cf0
Ubuntu <ana@ana-machine.gsc4kn5ep5bujc2fl04gwmbk4a.zx.internal.cloudapp.net>
2022-03-10 10:40:59 +0000

    Add create.py to deploy.sh

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -6,3 +6,5 @@ echo "Deploy stage"
 scp docker-compose.yaml jenkins@ana-swarm:/home/jenkins/docker-compose.yaml
 
 ssh jenkins@ana-swarm DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR DATABASE_URI=$DATABASE_URI docker stack deploy --compose-file docker-compose.yaml mpwa
+
+python3 create.py
