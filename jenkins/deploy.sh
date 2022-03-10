9a470a7226a9ae84acaece161e50f4176c919759
Ubuntu <ana@ana-machine.gsc4kn5ep5bujc2fl04gwmbk4a.zx.internal.cloudapp.net>
2022-03-10 09:18:23 +0000

    Changes to deploy.sh

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -5,5 +5,4 @@ echo "Deploy stage"
 # copying docker-compose.yaml to swarm machine
 scp docker-compose.yaml jenkins@ana-swarm:/home/jenkins/docker-compose.yaml
 
-ssh jenkins@ana-swarm \
-    DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR docker stack deploy --compose-file docker-compose.yaml mpwa
+ssh jenkins@ana-swarm DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR docker stack deploy --compose-file docker-compose.yaml mpwa
