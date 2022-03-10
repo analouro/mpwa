232f4debddfa9371ae779f728a3c8b6ad3fbfccc
Ubuntu <ana@ana-machine.gsc4kn5ep5bujc2fl04gwmbk4a.zx.internal.cloudapp.net>
2022-03-10 12:13:08 +0000

    Changes in layout.html + #'d create.py

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,9 +1,9 @@
 from application import app
-from application import db
-from application.models import User, Recipe 
+# from application import db
+# from application.models import User, Recipe 
 
-db.drop_all()
-db.create_all()
+# db.drop_all()
+# db.create_all()
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
