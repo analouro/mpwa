857693c734a81f17a4229bc97c63f3b7d9ed7ae2
Ubuntu <ana@ana-machine.gsc4kn5ep5bujc2fl04gwmbk4a.zx.internal.cloudapp.net>
2022-03-10 10:43:11 +0000

    Update path to create.py on deploy.sh

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -7,4 +7,4 @@ scp docker-compose.yaml jenkins@ana-swarm:/home/jenkins/docker-compose.yaml
 
 ssh jenkins@ana-swarm DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR DATABASE_URI=$DATABASE_URI docker stack deploy --compose-file docker-compose.yaml mpwa
 
-python3 create.py
+python3 flask-app/application/create.py
