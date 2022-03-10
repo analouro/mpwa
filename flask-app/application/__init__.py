15417cdb5dbfc355cfd0d3a46ddb822ba9e841a3
Ubuntu <ana@ana-machine.gsc4kn5ep5bujc2fl04gwmbk4a.zx.internal.cloudapp.net>
2022-03-10 15:01:43 +0000

    Added ENV to app dockerfile

--- flask-app/application/__init__.py
+++ flask-app/application/__init__.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 
 SECRET_KEY = os.urandom(32)
 app.config["SECRET_KEY"] = SECRET_KEY
