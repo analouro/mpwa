619e85c1f62f0247c8dcfa8658255ab65632468c
Ubuntu <ana@ana-machine.gsc4kn5ep5bujc2fl04gwmbk4a.zx.internal.cloudapp.net>
2022-03-10 13:18:52 +0000

    Changed app.py - db still not working + updated layout.html

--- flask-app/app.py
+++ flask-app/app.py
@@ -3,7 +3,7 @@ from application import db
 from application.models import User, Recipe 
 
 # db.drop_all()
-db.create_all()
+# db.create_all()
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
