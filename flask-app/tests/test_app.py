5a23231cfc83cec34d982e94c160dbf9291fdc6c
Ubuntu <ana@ana-machine.gsc4kn5ep5bujc2fl04gwmbk4a.zx.internal.cloudapp.net>
2022-03-10 14:10:33 +0000

    Added volumes section to mysql in docker-compose.yaml

--- flask-app/tests/test_app.py
+++ flask-app/tests/test_app.py
@@ -17,7 +17,8 @@ class TestBase(TestCase):
         db.drop_all()
         db.create_all()
 
-        user1 = User(name="MsTest")
+        user1 = User(name="MsTest", id="10172104")
+        recipe1 = Recipe(user_id="10172104", name="Testghetti")
         # I need to pass 2 bits of information for the recipe to be read: recipe name and user_id
         # recipe1 = Recipe(name="Testghetti")
         # recipe2 = Recipe(name="Testeroni")
@@ -26,7 +27,7 @@ class TestBase(TestCase):
         # recipe5 = Recipe(name="Testake")
 
         db.session.add(user1)
-        # db.session.add(recipe1)
+        db.session.add(recipe1)
         # db.session.add(recipe2)
         # db.session.add(recipe3)
         # db.session.add(recipe4)
@@ -47,7 +48,12 @@ class TestPages(TestBase):
         response = self.client.get(url_for('user'))
         self.assertEqual(response.status_code, 200)
 
-    def test_read(self):
+    def test_recipe(self):
         response = self.client.post(url_for('recipe'))
         self.assertIn(b'MsTest', response.data)
         # self.assertIn(b'Testghetti', response.data)
+    
+    def test_read(self):
+        response = self.client.get(url_for('read'))
+        self.assertIn(b'MsTest', response.data)
+        self.assertIn(b'Testghetti', response.data)
